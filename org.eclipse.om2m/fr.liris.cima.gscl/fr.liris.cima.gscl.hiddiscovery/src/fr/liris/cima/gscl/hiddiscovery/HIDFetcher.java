f40d3b1b37d84bd64e556ffa05a8fdc6a7eada5f
harold <devnull@localhost>
2014-11-27 21:38:47 +0100

    Intégration du code des ptut

---
+++ org.eclipse.om2m/fr.liris.cima.gscl/fr.liris.cima.gscl.hiddiscovery/src/fr/liris/cima/gscl/hiddiscovery/HIDFetcher.java
@@ -0,0 +1,73 @@
+/*
+
+Recupere les infos HID et enleve les info inutiles (lsusb -v = VERBEUX !)
+
+*/
+package fr.liris.cima.gscl.hiddiscovery;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import static java.lang.System.exit;
+
+
+public class HIDFetcher {
+    public HIDFetcher(){
+        
+    }
+    public String fetch(int bus, int numPeriph){
+        String out = "";
+        String temp = "";
+        try {
+            Process p = Runtime.getRuntime().exec("lsusb -s "+bus+":"+numPeriph+" -v");
+             
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            //BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+ 
+            
+            while ((temp = stdInput.readLine()) != null) {
+                out = out + temp+"\n";
+            }
+             //System.out.println(out);
+        }
+        catch (IOException e) {
+            System.out.println("Exception : HIDFetcher:fetch:");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        out = filterRawOutput(out);
+        return out;
+    }
+    public String filterRawOutput(String rawInput){//Supprime le debut de lsusb ( 0 -> "Device descriptor")
+        String out = "";
+        System.out.println("Beginning filtering...");
+        String startSymbol = "Device Descriptor:";
+        System.out.println("StartFilter : \'"+startSymbol+"\'");
+        int i = 0;
+        while(i < rawInput.length()-startSymbol.length()){
+            //System.out.println("Current substring : "+rawInput.substring(i, i+startSymbol.length()));
+            
+            if(rawInput.substring(i, i+startSymbol.length()).equals(startSymbol)){
+                System.out.println("Symbol find at : "+i);
+                out = StrRemoveAt(rawInput,0,i);
+                i = rawInput.length()+1;
+            }
+            i++;
+        }
+        
+        
+        return out;
+    }
+    
+    public String StrRemoveAt(String str,int posDebut,int n){
+        String temp;
+        String out = str;
+        temp = out.substring(posDebut, posDebut+n);
+        //System.out.println("Will delete : "+temp);
+        out = out.replace(temp,"");
+        return out;
+    }
+
+
+}
+
---
+++ org.eclipse.om2m/fr.liris.cima.gscl/fr.liris.cima.gscl.hiddiscovery/src/fr/liris/cima/gscl/hiddiscovery/PIUX.java
@@ -0,0 +1,37 @@
+/*
+
+(Bus:IDPeriph) => HIDfetcher => Parser => XMLizer => <3
+
+*/
+
+
+package fr.liris.cima.gscl.hiddiscovery;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+
+
+public class PIUX{
+    private static boolean b =true;
+  
+    public static void main(String[] args) {
+   /*     System.out.println("PIUX\n\n");
+        
+        HIDFetcher fetcher = new HIDFetcher();
+        String outf = fetcher.fetch(2,4);
+        //System.out.println("\n\nOUT HIDFetcher:\n"+outf);
+        
+        Parser parse = new Parser();
+        String outp = parse.parse(outf);
+        
+        System.out.println("Parser : "+outp);
+        
+        XMLizer xml = new XMLizer();
+        String outx = xml.xmlize(outp);
+        System.out.println("XMLizer : "+outx);
+ */       
+    }
+    
+}
