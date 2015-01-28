96ae6a0ce98250e43f0be425ac4aa9bffbbe4c94
Rémi Desmargez <devnull@localhost>
2015-01-28 10:07:58 +0100

    change manual configuration to cima administration

--- org.eclipse.om2m/org.eclipse.om2m.webapp.resourcesbrowser/src/main/java/org/eclipse/om2m/webapp/resourcesbrowser/CIMAAdministrationServlet.java
+++ org.eclipse.om2m/org.eclipse.om2m.webapp.resourcesbrowser/src/main/java/org/eclipse/om2m/webapp/resourcesbrowser/CIMAAdministrationServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ManualConfigServlet extends HttpServlet {
+public class CIMAAdministrationServlet extends HttpServlet {
     /** Serial Version UID */
     private static final long serialVersionUID = 1L;
       @Override
