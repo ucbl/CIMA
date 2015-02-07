d6e787768f5e9c09c49f2d9208d9abb9c151333e
Maxime Baudin <devnull@localhost>
2015-02-07 12:33:19 +0100

    InterfaceClient -> Ajout du module ngToast. Retour de requêtes par notifications.

--- org.eclipse.om2m/org.eclipse.om2m.webapp.resourcesbrowser/src/main/resources/webapps/cima/js/app.js
+++ org.eclipse.om2m/org.eclipse.om2m.webapp.resourcesbrowser/src/main/resources/webapps/cima/js/app.js
@@ -1,5 +1,5 @@
 /* Application moduls */
-var app = angular.module("MonApp",["MonApp.routes","MonApp.ui","MonApp.tags"]);
+var app = angular.module("MonApp",["MonApp.routes","MonApp.ui","MonApp.tags","MonApp.toast"]);
 
 /*routing URLs*/
 angular.module('MonApp.routes', ['ngRoute']).config(function($routeProvider){
@@ -12,6 +12,15 @@ angular.module('MonApp.routes', ['ngRoute']).config(function($routeProvider){
 /* bootstrap UI modul */
 angular.module('MonApp.ui', ['ui.bootstrap']);
 angular.module('MonApp.tags', ['ngTagsInput']);
+angular.module('MonApp.toast', ['ngToast'])
+  .config(['ngToastProvider', function(ngToast) {
+    ngToast.configure({
+      verticalPosition: 'top',
+      horizontalPosition: 'right',
+      dismissButton: true,
+      maxNumber: 3,
+    });
+  }]);
 
 /*Auth header for http requests*/
 app.run(function($http){
