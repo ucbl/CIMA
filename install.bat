5fed45912424177db905c058ab431c84f06a81a8
p1209585 <devnull@localhost>
2015-07-08 10:52:07 +0200

    "ajout des fichiers d'installation et de lancement sous windows"

---
+++ install.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "org.eclipse.om2m"
+mvn clean install
---
+++ launchCimaBatch.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "org.eclipse.om2m\org.eclipse.om2m.site.gscl\target\products\gscl\win32\win32\x86_64" & Start gscl.exe
+echo %time%
+timeout 2 > NUL
+echo %time%
+cd "..\..\..\..\..\..\..\org.eclipse.om2m.site.nscl\target\products\nscl\win32\win32\x86_64" & Start nscl.exe
