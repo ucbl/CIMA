bdae0f6c4549c53b726f264dfdce08ac1d75013e
Maxime <devnull@localhost>
2015-10-07 22:55:42 +0200

    Ajout d'un fichier de lancement pour mac.

---
+++ macBuildAndStart.sh
@@ -0,0 +1,8 @@
+
+pwd=`pwd`
+sudo mvn clean install
+wait
+
+osascript -e "tell application \"Terminal\" to do script \"cd $pwd; clear; sudo java -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true org.eclipse.om2m/org.eclipse.om2m.site.gscl/target/products/gscl/macosx/cocoa/x86_64/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit\""> /dev/null
+
+osascript -e "tell application \"Terminal\" to do script \"cd $pwd; clear; sudo java -jar -ea -Declipse.ignoreApp=true -Dosgi.clean=true -Ddebug=true org.eclipse.om2m/org.eclipse.om2m.site.nscl/target/products/nscl/macosx/cocoa/x86_64/plugins/org.eclipse.equinox.launcher_1.3.0.v20140415-2008.jar -console -noExit\""> /dev/null
