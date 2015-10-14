f13e44d234bf8fcbe79eb975cfa5e65d42bed1af
Ricardo <devnull@localhost>
2015-10-14 11:21:43 +0200

    amelioration de la fonction de réseau decouverte

--- org.eclipse.om2m/fr.liris.cima.gscl/fr.liris.cima.gscl.commons/src/main/java/fr/liris/cima/gscl/commons/IPFinder.java
+++ org.eclipse.om2m/fr.liris.cima.gscl/fr.liris.cima.gscl.commons/src/main/java/fr/liris/cima/gscl/commons/IPFinder.java
@@ -4,10 +4,14 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.net.InterfaceAddress;
+import org.apache.commons.net.util.SubnetUtils;
+import org.apache.commons.validator.routines.InetAddressValidator;
 import java.util.Enumeration;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.List;
 
 /**
  * The IPFinder TimerTask class look for all the available IP that can be found.
@@ -28,40 +32,66 @@ public class IPFinder extends TimerTask {
             lne = NetworkInterface.getNetworkInterfaces();
 
             //Iterate all the networks interfaces (wlan0 en0 wlan1 eth1 ...)
-            while(lne.hasMoreElements()) {
+            while (lne.hasMoreElements()) {
 
                 NetworkInterface networkInterface = lne.nextElement();
 
                 Enumeration<InetAddress> ia = networkInterface.getInetAddresses();
+                List<InterfaceAddress> inter = networkInterface.getInterfaceAddresses();
 
+                int cnt = 0;
                 //Iterate all locals IP in this network interface
-                while(ia.hasMoreElements()) {
+                while (ia.hasMoreElements()) {
                     InetAddress inetAddress = ia.nextElement();
+                    InterfaceAddress address = inter.get(cnt++);
+                    String myAddress = address.getAddress().getHostAddress();
 
-                    //Only if this is a localSite IP, we can look for other ips
-                    if(inetAddress.isSiteLocalAddress())
-                    {
-                        String myAdress = inetAddress.getHostAddress();
-                        String myAdressSplited[] = myAdress.split("\\.");
-                        String adressStart = myAdressSplited[0] +"."+ myAdressSplited[1]+"." + myAdressSplited[2]+".";
+                    //IPV4 verification and if it's not the localhost IP (127.0.0.1)
+                    if (InetAddressValidator.getInstance().isValidInet4Address(myAddress) && !myAddress.equals("127.0.0.1")) {
+                        SubnetUtils networkInfo = new SubnetUtils(myAddress + "/" + address.getNetworkPrefixLength());
 
-                        //We try all ips started by myIp and ended by [0,255] (yes, myIP too)
-                        for(int i = 0; i!= 256; i++)
-                        {
+                        //Only if this is a localSite IP, we can look for other ips
+                        if (inetAddress.isSiteLocalAddress()) {
+                            
+                            String currentAddress = networkInfo.getInfo().getLowAddress();
+                            String adressSplited[] = currentAddress.split("\\.");
+                            String broadcastAddress = networkInfo.getInfo().getBroadcastAddress();
 
-                            String adder = adressStart + i;
-                            if (!adder.equals(myAdress))
-                                threadPool.submit(new IPChecker(this, adder));
+                            //We try all IP addresses in the same network as myAddress
+                            while (!currentAddress.equals(broadcastAddress)) {
+
+                                if (!currentAddress.equals(myAddress)) {
+                                    threadPool.submit(new IPChecker(this, currentAddress));
+                                }
+
+                                int b4 = Integer.parseInt(adressSplited[3]) + 1;
+                                int b3 = Integer.parseInt(adressSplited[2]);
+                                int b2 = Integer.parseInt(adressSplited[1]);
+                                int b1 = Integer.parseInt(adressSplited[0]);
+
+                                if (b4 == 256) {
+                                    b4 = 0;
+                                    b3++;
+                                    if (b3 == 256) {
+                                        b3 = 0;
+                                        b2++;
+                                        if (b2 == 256) {
+                                            b2 = 0;
+                                            b1++;
+                                        }
+                                    }
+                                }
+                                currentAddress = "" + b1 + "." + b2 + "." + b3 + "." + b4;
+                                adressSplited = currentAddress.split("\\.");
+                            }
                         }
                     }
                 }
-
             }
         } catch (SocketException e) {
             //TODO
             e.printStackTrace();
         }
-
     }
 
     /**
