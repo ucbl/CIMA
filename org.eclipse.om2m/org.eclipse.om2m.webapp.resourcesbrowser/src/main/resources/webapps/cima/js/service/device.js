141ecf26151406f9828f7b9b4695920863834503
p1114753 <devnull@localhost>
2015-10-20 10:43:19 +0200

    Reset service/device.js

--- org.eclipse.om2m/org.eclipse.om2m.webapp.resourcesbrowser/src/main/resources/webapps/cima/js/service/device.js
+++ org.eclipse.om2m/org.eclipse.om2m.webapp.resourcesbrowser/src/main/resources/webapps/cima/js/service/device.js
@@ -58,25 +58,25 @@ app.factory('DeviceFactory', ['$http', '$q', function($http, $q){
         /*test a device*/
         testCapability : function(idDevice, capability){
             var deferred = $q.defer();
-            // $http({
-            //     //url : URL_DEVICE + '/' + idDevice+'/test',    
-            //     url: URL_DEVICE + '/' + idDevice+'/test',
-            //     method: "POST",
-            //     data: capability,
-            //     headers: {'Content-Type': 'application/json'}
-            // }).success(function (data, status, headers, config) {
-            //         deferred.resolve();
-            // }).error(function (data, status, headers, config) {
-            //         deferred.reject('Unable to test capability, status : '+status+', header : '+headers);
-            // });
-            
-            $http.post(URL_DEVICE + '/' + idDevice + '/test', {
-                data: capability
-            }).then(function(response) {
-                deferred.resolve(response.data);
-            }, function(error) {
-                deferred.reject('Unable to test capability, error : ' + error);
+            $http({
+                //url : URL_DEVICE + '/' + idDevice+'/test',    
+                url: URL_DEVICE + '/' + idDevice+'/test',
+                method: "POST",
+                data: capability,
+                headers: {'Content-Type': 'application/json'}
+            }).success(function (data, status, headers, config) {
+                    deferred.resolve();
+            }).error(function (data, status, headers, config) {
+                    deferred.reject('Unable to test capability, status : '+status+', header : '+headers);
             });
+            
+            // $http.post(URL_DEVICE + '/' + idDevice + '/test', {
+            //     data: capability
+            // }).then(function(response) {
+            //     deferred.resolve(response.data);
+            // }, function(error) {
+            //     deferred.reject('Unable to test capability, error : ' + error);
+            // });
             return deferred.promise;
         },
 
@@ -100,75 +100,75 @@ app.factory('DeviceFactory', ['$http', '$q', function($http, $q){
             //     deferred.reject('Unable to add capability, status : '+status+', header : '+headers);
             // });
             
-            // $http.put(URL_DEVICE + '/' + idDevice + '/capability/' + capability.id + '/', {
-            //     data: capability
-            // }).
-            // success(function(data, status, headers, config) {
-            //     deferred.resolve();
-            // }).
-            // error(function(data, status, headers, config) {
-            //     console.log(JSON.stringify(data));
-            //     deferred.reject('Unable to add capability, status : '+status+', header : '+headers);
-            // });
-            
             $http.put(URL_DEVICE + '/' + idDevice + '/capability/' + capability.id + '/', {
                 data: capability
-            }).then(function(response) {
-                deferred.resolve(response.data);
-            }, function(error) {
-                deferred.reject('Unable to add capability, error : ' + error);
+            }).
+            success(function(data, status, headers, config) {
+                deferred.resolve();
+            }).
+            error(function(data, status, headers, config) {
+                console.log(JSON.stringify(data));
+                deferred.reject('Unable to add capability, status : '+status+', header : '+headers);
             });
+            
+            // $http.put(URL_DEVICE + '/' + idDevice + '/capability/' + capability.id + '/', {
+            //     data: capability
+            // }).then(function(response) {
+            //     deferred.resolve(response.data);
+            // }, function(error) {
+            //     deferred.reject('Unable to add capability, error : ' + error);
+            // });
             return deferred.promise;
         },
 
         /*save a device (server side)*/
         saveDevice : function(device){
             var deferred = $q.defer();
-            // $http({
-            //     url: URL_DEVICE+'/'+device.id+'/',
-            //     method: "PUT",
-            //     data: device,
-            //     headers: {'Content-Type': 'application/json'}
-            // }).success(function (data, status, headers, config) {
-            //     deferred.resolve();
-            // }).error(function (data, status, headers, config) {
-            //     //deferred.reject('Unable to save device, status : '+status+', header : '+headers);
-            //     deferred.resolve();
-            // });
-           
-            $http.put(URL_DEVICE+'/'+device.id+'/', {
-                data: device
-            }).then(function(response) {
-                deferred.resolve(response.data);
-            }, function(error) {
-                deferred.reject('Unable to save device, error : ' + error);
+            $http({
+                url: URL_DEVICE+'/'+device.id+'/',
+                method: "PUT",
+                data: device,
+                headers: {'Content-Type': 'application/json'}
+            }).success(function (data, status, headers, config) {
+                deferred.resolve();
+            }).error(function (data, status, headers, config) {
+                //deferred.reject('Unable to save device, status : '+status+', header : '+headers);
+                deferred.resolve();
             });
+           
+            // $http.put(URL_DEVICE+'/'+device.id+'/', {
+            //     data: device
+            // }).then(function(response) {
+            //     deferred.resolve(response.data);
+            // }, function(error) {
+            //     deferred.reject('Unable to save device, error : ' + error);
+            // });
             return deferred.promise;
         },
 
         /*modifying capability*/
         modifyCapability : function(idDevice, capability){
             var deferred = $q.defer();
-            // $http({
-            //     url: URL_DEVICE+'/'+idDevice+'/capability/'+capability.id+"/",
-            //     method: "PUT",
-            //     data: capability,
-            //     headers: {'Content-Type': 'application/json'}
-            // }).success(function (data, status, headers, config) {
-            //     deferred.resolve();
-            //     alert('success');
-            // }).error(function (data, status, headers, config) {
-            //     deferred.reject('Unable to modify capability, status : '+status+', header : '+headers);
-            // });
-            
-            
-            $http.put(URL_DEVICE + '/' + idDevice + '/capability/' + capability.id + '/', {
-                data: capability
-            }).then(function(response) {
-                deferred.resolve(response.data);
-            }, function(error) {
-                deferred.reject('Unable to modify capability, error : ' + error);
+            $http({
+                url: URL_DEVICE+'/'+idDevice+'/capability/'+capability.id+"/",
+                method: "PUT",
+                data: capability,
+                headers: {'Content-Type': 'application/json'}
+            }).success(function (data, status, headers, config) {
+                deferred.resolve();
+                alert('success');
+            }).error(function (data, status, headers, config) {
+                deferred.reject('Unable to modify capability, status : '+status+', header : '+headers);
             });
+            
+            
+            // $http.put(URL_DEVICE + '/' + idDevice + '/capability/' + capability.id + '/', {
+            //     data: capability
+            // }).then(function(response) {
+            //     deferred.resolve(response.data);
+            // }, function(error) {
+            //     deferred.reject('Unable to modify capability, error : ' + error);
+            // });
             return deferred.promise;
         },
 
@@ -186,21 +186,21 @@ app.factory('DeviceFactory', ['$http', '$q', function($http, $q){
             //     deferred.reject('Unable to delete capability, status : '+status+', header : '+headers);
             // });
             
-            // $http.delete(URL_DEVICE + '/' + idDevice + '/capability/' + idCapability + '/').
-            // success(function(data, status, headers, config) {
-            //     deferred.resolve();
-            // }).
-            // error(function(data, status, headers, config) {
-                
-            //     deferred.reject('Unable to delete capability, status : '+status+', header : '+headers);
-            // });
-            
             $http.delete(URL_DEVICE + '/' + idDevice + '/capability/' + idCapability + '/').
-            then(function(response) {
-                deferred.resolve(response.data);
-            }, function(error) {
-                deferred.reject('Unable to delete capability, error : ' + error);
+            success(function(data, status, headers, config) {
+                deferred.resolve();
+            }).
+            error(function(data, status, headers, config) {
+                
+                deferred.reject('Unable to delete capability, status : '+status+', header : '+headers);
             });
+            
+            // $http.delete(URL_DEVICE + '/' + idDevice + '/capability/' + idCapability + '/').
+            // then(function(response) {
+            //     deferred.resolve(response.data);
+            // }, function(error) {
+            //     deferred.reject('Unable to delete capability, error : ' + error);
+            // });
             return deferred.promise;
         }
 
